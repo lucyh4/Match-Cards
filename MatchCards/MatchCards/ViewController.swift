e0c2a89213079d18200e7ace7bed76a125204192
Neha <neha.mcs.du.2015@gmail.com>
2020-05-13 01:39:24 +0530

    Add buttons with flip functionality

--- MatchCards/MatchCards/ViewController.swift
+++ MatchCards/MatchCards/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+
+    @IBOutlet weak var flipLabel: UILabel!
+    @IBOutlet var arrOfCards: [UIButton]!
+    
+    //MARK: - Array of emojis
+    let arrayOfEmojis = ["😇", "😈", "👻", "🎃"]
+    
+    
+    //MARK: - Creating var for no. of flips with didset property
+      var numOfFlips = 0 {
+          didSet{
+              flipLabel.text = "Flips: \(numOfFlips)"
+          }
+      }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cardClicked(_ sender: UIButton) {
+        numOfFlips+=1
+        if let cardNumber = arrOfCards.firstIndex(of: sender){
+            flipCard(wihtEmoji: arrayOfEmojis[cardNumber], on: sender)
+        }
+        
+    }
+    
+    //MARK: - Function to flip the card
+    func flipCard(wihtEmoji emoji : String, on button : UIButton){
+        if emoji == button.currentTitle{
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.1425348818, green: 0.5279285312, blue: 0.9669786096, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            
+        }
+    }
+    
 }
 
